4b27f26ee8afd40636f0611df912a2a5009c2277
Andrew Boles <andrew@bolesdata.consulting>
2025-02-22 13:29:12 -0600

    Create service-worker.js

---
+++ static/service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "wind-chill-cache-v1";
+const urlsToCache = [
+  "/",
+  "/static/manifest.json",
+  "/static/service-worker.js",
+  "/static/icons/icon-192.png",
+  "/static/icons/icon-512.png"
+];
+
+// Install the service worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch files from the cache
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate the new service worker and clear old caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+});
